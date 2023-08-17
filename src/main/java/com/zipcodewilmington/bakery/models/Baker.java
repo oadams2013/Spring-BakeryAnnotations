b8ebe2ccd75003de783172d001b51d33c175d327
olamide <oadams@fandm.edu>
2023-08-17 16:03:02 -0400

    baker class complete

--- src/main/java/com/zipcodewilmington/bakery/models/Baker.java
+++ src/main/java/com/zipcodewilmington/bakery/models/Baker.java
@@ -1,9 +1,17 @@
 package com.zipcodewilmington.bakery.models;
 
 
-import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+@Entity
 public class Baker {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
@@ -16,6 +24,7 @@ public class Baker {
     }
 
     public Baker(String name, String employeeId, String specialty) {
+
         this(null, name, employeeId, specialty);
     }
 
